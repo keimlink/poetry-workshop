4a0719b5d881a019212d4d1a7c757acb3697d588
Markus Zapke-Gründemann <markus@keimlink.de>
2021-10-10 22:57:04 +0200

    src: Add command to print current date and time for a timezone

--- src/poetry-demo/src/poetry_demo/__init__.py
+++ src/poetry-demo/src/poetry_demo/__init__.py
@@ -1,1 +1,24 @@
 __version__ = '0.1.0'
+
+import argparse
+
+import pendulum
+
+
+def parse():
+    parser = argparse.ArgumentParser(description="Print current date and time for a timezone")
+    parser.add_argument("-tz" ,"--timezone", default="UTC")
+    return parser.parse_args()
+
+
+def now(timezone):
+    return pendulum.now(timezone)
+
+
+def main():
+    args = parse()
+    print(now(args.timezone))
+
+
+if "__name__" == "__main__":
+    main()
